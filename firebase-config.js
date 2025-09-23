7317a481476694354cfbf558472c9767dad553be
t4w4ntm <122960758+t4w4ntm@users.noreply.github.com>
2025-09-23 23:00:56 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+
+// === FILL YOUR FIREBASE CONFIG HERE ===
+// Get this from Firebase console -> Project settings -> General -> Your apps -> SDK setup and configuration
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCmxWWFAJBI7sXT85F8juxE7pRUXAm11ag",
+  authDomain: "kahoot-32919.firebaseapp.com",
+  projectId: "kahoot-32919",
+  storageBucket: "kahoot-32919.appspot.com",
+  messagingSenderId: "748671300698",
+  appId: "1:748671300698:web:1456521c7cd6f89da15463",
+};
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmxWWFAJBI7sXT85F8juxE7pRUXAm11ag",
+  authDomain: "kahoot-32919.firebaseapp.com",
+  projectId: "kahoot-32919",
+  storageBucket: "kahoot-32919.firebasestorage.app",
+  messagingSenderId: "748671300698",
+  appId: "1:748671300698:web:1456521c7cd6f89da15463",
+  measurementId: "G-F0LBLVWC10"
+};
